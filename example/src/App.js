626b9680d8c9655fd692f9ea5cbc9fbb7b0ecd4b
xinkule <ccnualbertzhong@163.com>
2019-11-08 00:32:53 +0800

    [feat] 0.2.0 example

--- example/src/App.js
+++ example/src/App.js
@@ -1,43 +1,45 @@
-import * as React from 'react';
-import { hot } from 'react-hot-loader';
-import Toast from '../../src';
-import './index.css';
+import React from 'react';
+import Toast from 'light-toast';
 
-class App extends React.Component {
-  handleClick = type => {
-    Toast[type](type);
-    if (type === 'loading') {
-      setTimeout(() => {
-        Toast.hide();
-      }, 3000);
-    }
-  };
-
-  render() {
-    return (
-      <div className="container">
-        <h2>Light Toast</h2>
-        <span className="button" onClick={this.handleClick.bind(this, 'info')}>
-          info
-        </span>
-        <span
-          className="button"
-          onClick={this.handleClick.bind(this, 'success')}
-        >
-          success
-        </span>
-        <span className="button" onClick={this.handleClick.bind(this, 'fail')}>
-          fail
-        </span>
-        <span
-          className="button"
-          onClick={this.handleClick.bind(this, 'loading')}
-        >
-          loading
-        </span>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="container">
+      <h2>Light Toast</h2>
+      <span
+        className="button"
+        onClick={() => {
+          Toast.info('info');
+        }}
+      >
+        info
+      </span>
+      <span
+        className="button"
+        onClick={() => {
+          Toast.success('success');
+        }}
+      >
+        success
+      </span>
+      <span
+        className="button"
+        onClick={() => {
+          Toast.fail('fail');
+        }}
+      >
+        fail
+      </span>
+      <span
+        className="button"
+        onClick={() => {
+          Toast.loading('loading');
+          setTimeout(() => {
+            Toast.hide();
+          }, 3000);
+        }}
+      >
+        loading
+      </span>
+    </div>
+  );
 }
-
-export default hot(module)(App);
